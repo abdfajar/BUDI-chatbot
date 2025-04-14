4811951bea99207ac6d774ab65bd20ea2b270b91
abdfajar <abd.fajar@gmail.com>
2025-04-14 21:04:40 +0700

    Update layout.py

--- src/modules/layout.py
+++ src/modules/layout.py
@@ -33,7 +33,7 @@ class Layout:
         with st.form(key="my_form", clear_on_submit=True):
             user_input = st.text_area(
                 "Query:",
-                placeholder="Ask me anything about the document...",
+                placeholder="Silakan tanya tentang dokumen terlampir...",
                 key="input",
                 label_visibility="collapsed",
             )
